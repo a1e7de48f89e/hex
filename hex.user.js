0960bff170e02c3b2a47e805ab3747f4ca0b3461
a1e7de48f89e <28833155+a1e7de48f89e@users.noreply.github.com>
2019-12-26 04:46:48 +0100

    docs: update changelog and version numbers for release 1.2.0

--- hex.user.js
+++ hex.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name hex
-// @version 1.1.0
+// @version 1.2.0
 // @author a1e7de48f89e
 // @description Userscript for hex, a hydrus API plugin to download ExH archives
 // @website https://github.com/a1e7de48f89e/hex
